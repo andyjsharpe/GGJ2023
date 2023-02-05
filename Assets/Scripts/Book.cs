da0f4fa940cf07d07e541074a9f4e95d92a10579
DinoSage <dinosage5632@gmail.com>
2023-02-05 14:50:46 -0500

    Changed "Boy" to child in story

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -4,46 +4,46 @@ using UnityEngine;
 
 public class Book
 {
-    public static string day1OLD = "Once upon a time, there was a young boy who had a deep passion for bears. " +
-        "He had books and toys that featured these magnificent creatures and would spend hours learning " +
-        "about them and imagining himself in their world. He would often daydream about wandering through" +
-        " the forest and coming across a family of bears, with whom he would have wondrous adventures.\r\n" +
-        "The boy's love for bears continued to grow as he got older, and he made it his goal to one day visit " +
-        "a bear sanctuary where he could observe and learn even more about these magnificent animals. The" +
-        " young boy's love for bears was about to reach new heights.";
+    public static string day1OLD = "Once upon a time, there was a young child who had a deep passion for bears. " +
+        "They had books and toys that featured these magnificent creatures and would spend hours learning " +
+        "about them and imagining themself in their world. They would often daydream about wandering through" +
+        " the forest and coming across a family of bears, with whom they would have wondrous adventures.\r\n" +
+        "The child's love for bears continued to grow as they got older, and they made it their goal to one day visit " +
+        "a bear sanctuary where they could observe and learn even more about these magnificent animals. The" +
+        " young child's love for bears was about to reach new heights.";
 
-    public static string day2 = "Once upon a time, there was a young boy who had a deep passion for bears. " +
-        "He had books and toys that featured these magnificent creatures and would spend hours learning about " +
-        "them and imagining himself in their world. He would often daydream about wandering through the forest " +
-        "and coming across a family of bears, with whom he would have wondrous adventures.\r\nThe boy's love " +
-        "for bears continued to grow as he got older, and he made it his goal to one day visit a bear sanctuary " +
-        "where he could observe and learn even more about these magnificent animals.";
+    public static string day2 = "Once upon a time, there was a young child who had a deep passion for bears. " +
+        "They had books and toys that featured these magnificent creatures and would spend hours learning about " +
+        "them and imagining themself in their world. They would often daydream about wandering through the forest " +
+        "and coming across a family of bears, with whom they would have wondrous adventures.\r\nThe child's love " +
+        "for bears continued to grow as they got older, and they made it their goal to one day visit a bear sanctuary " +
+        "where they could observe and learn even more about these magnificent animals.";
 
-    public static string day3 = "The young boy's love for bears was about to reach new heights. The young boy and " +
+    public static string day3 = "The young child's love for bears was about to reach new heights. The young child and " +
         "his parent, dedicated to a career in office management, shared a love for gardening, and they often spent " +
-        "their weekends tending to their garden. The boy had a special fondness for the sunflower he had planted, " +
-        "and he took great care in making sure it received enough sunlight and water to grow tall and strong. From " +
-        "that day forward, the young boy and his parent continued to work on their garden, planting new flowers and " +
-        "vegetables and watching them grow, but the sunflower remained as the boy's favorite.";
+        "their weekends tending to their garden. The child had a special fondness for the sunflower they had planted, " +
+        "and they took great care in making sure it received enough sunlight and water to grow tall and strong. From " +
+        "that day forward, the young child and his parent continued to work on their garden, planting new flowers and " +
+        "vegetables and watching them grow, but the sunflower remained as the child's favorite.";
 
-    public static string day4 = "The boy’s parent took notice of the boy's fascination with bears and took him on a " +
-        "special trip to a bear sanctuary. It was a dream come true for the boy, who had always wanted to see bears" +
-        " up close.\r\nAs soon as they arrived, the boy's eyes lit up with excitement. He was surrounded by" +
-        " bears of all sizes and varieties, from tiny cubs to large and majestic adult bears. The boy was in awe " +
-        "as he watched them roam, play and interact with each other. He learned so much from the experienced guides " +
+    public static string day4 = "The child’s parent took notice of the kid's fascination with bears and took them on a " +
+        "special trip to a bear sanctuary. It was a dream come true for the child, who had always wanted to see bears" +
+        " up close.\r\nAs soon as they arrived, the child's eyes lit up with excitement. They was surrounded by" +
+        " bears of all sizes and varieties, from tiny cubs to large and majestic adult bears. The child was in awe " +
+        "as they watched them roam, play and interact with each other. They learned so much from the experienced guides " +
         "and caretakers who were on hand to answer his many questions.";
 
-    public static string day5 = "The young boy's visit to the bear sanctuary took a dangerous turn when he got too " +
-        "close to the bear pen. In his excitement to see the bears up close, he accidentally lost his footing and " +
-        "fell into the enclosure. Meanwhile, the boy's parent was distracted by a call from work and was not paying" +
+    public static string day5 = "The young child's visit to the bear sanctuary took a dangerous turn when they got too " +
+        "close to the bear pen. In his excitement to see the bears up close, they accidentally lost their footing and " +
+        "fell into the enclosure. Meanwhile, the child's parent was distracted by a call from work and was not paying" +
         " attention to their child's whereabouts.\r\nThe caretakers at the sanctuary quickly sprang into action, " +
-        "but the boy was too close to the bears for them to safely retrieve him. As the bears closed in, the boy thought" +
-        " that this would be the end of his love affair with these magnificent creatures.";
+        "but the child was too close to the bears for them to safely retrieve them. As the bears closed in, the child thought" +
+        " that this would be the end of their love affair with these magnificent creatures.";
 
     public static string day6 = "But then, something miraculous happened. The mother bear who had been nursing her " +
-        "cubs approached the boy, sniffed him and then gently nuzzled him with her nose. The boy was stunned as the " +
+        "cubs approached the kid, sniffed them and then gently nuzzled them with her nose. The child was stunned as the " +
         "bear calmly assessed the situation and then retreated back to her cubs, allowing the caretakers to safely " +
-        "retrieve the boy from the enclosure.\r\nThe young boy was shaken but unharmed, and he left the sanctuary " +
+        "retrieve the child from the enclosure.\r\nThe young child was shaken but unharmed, and they left the sanctuary " +
         "with a new appreciation for the gentleness and intelligence of these magnificent creatures.";
 
 }
